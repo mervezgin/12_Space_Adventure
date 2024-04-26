a379a355585a05236557f7d194adcb48a2fbe013
mervezgin <mervezgin@gmail.com>
2024-04-26 15:38:32 +0300

    continued the background

---
+++ Assets/Scripts/BackgroundControl.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundControl : MonoBehaviour
+{
+    float backgroundPosition;
+    float distance = 10.24f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundPosition = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (backgroundPosition + distance < Camera.main.transform.position.y)
+        {
+            BackgroundPlace();
+        }
+    }
+
+    void BackgroundPlace()
+    {
+        backgroundPosition += distance * 2.0f;
+        Vector2 newPosition = new Vector2(0, backgroundPosition);
+        transform.position = newPosition;
+    }
+}
