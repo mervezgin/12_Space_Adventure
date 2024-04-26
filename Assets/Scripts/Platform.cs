aa3a2ddf4b7c8bff5937b0c93a558ff5c84e7bfa
mervezgin <mervezgin@gmail.com>
2024-04-26 17:29:39 +0300

    fixed platforms exiting screen

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     PolygonCollider2D polygonCollider2D;
+
     float randomSpeed;
     bool move = true;
+    float min, max;
+
     public bool Move { get { return move; } set {move = value; } }
 
     // Start is called before the first frame update
@@ -14,6 +17,19 @@ public class Platform : MonoBehaviour
     {
         polygonCollider2D = GetComponent<PolygonCollider2D>();
         randomSpeed = Random.Range(0.5f, 1.0f);
+
+        float objectWidth = polygonCollider2D.bounds.size.x / 2;
+
+        if (transform.position.x > 0)
+        {
+            min = objectWidth;
+            max = ScreenCalculator.instance.Width - objectWidth;
+        }
+        else
+        {
+            min = -ScreenCalculator.instance.Width + objectWidth;
+            max = -objectWidth;
+        }
     }
 
     // Update is called once per frame
@@ -21,7 +37,7 @@ public class Platform : MonoBehaviour
     {
         if (move)
         {
-            float pingPongX = Mathf.PingPong(Time.time * randomSpeed, 3.0f);
+            float pingPongX = Mathf.PingPong(Time.time * randomSpeed, max - min) + min;
             Vector2 pingPong = new Vector2(pingPongX, transform.position.y);
             transform.position = pingPong;
         }
